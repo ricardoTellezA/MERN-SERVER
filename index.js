c514b6c9e7684c0cb4421599121f032b785ec0ee
ricardoTellezA <lian.redi@hotmail.com>
2022-09-02 23:50:21 -0500

    m

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const moongose = require("mongoose");
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./gql/schema");
+const express = require("express");
 const resolvers = require("./gql/resolvers");
+const cors = require("cors");
 require("dotenv").config({
   path: ".env",
 });
+const app = express();
 
+// CONFIGURE CORS
+const whiteList = [
+  "https://limitless-crag-81698.herokuapp.com/",
+  "http://localhost:4000/",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    const existe = whiteList.some((dominio) => dominio === origin);
+    if (existe) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 moongose.connect(
   process.env.BBDD,
+
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -31,5 +51,9 @@ function server() {
 
   serverApollor.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
+
+    // CORS
   });
 }
+
+// USAR CORS
