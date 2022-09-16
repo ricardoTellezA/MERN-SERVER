ef1a82e55928c050cf0127a7de5581e02c0ecc03
ricardoTellezA <lian.redi@hotmail.com>
2022-09-15 22:31:12 -0500

    Algunos arreglos

--- index.js
+++ index.js
@@ -1,52 +1,18 @@
-const moongose = require("mongoose");
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./gql/schema");
-const express = require("express");
 const resolvers = require("./gql/resolvers");
-const cors = require("cors");
+const conectarDB = require("./config/db");
 require("dotenv").config({
   path: ".env",
 });
-const app = express();
 
-// CONFIGURE CORS
-//  corsOptions = {
-//   origin: "https://limitless-crag-81698.herokuapp.com/",
-//   optionsSuccessStatus: 200,
-  
-//  };
-moongose.connect(
-  process.env.BBDD,
+conectarDB();
 
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: true,
-    // useCreateIndex: true,
-  },
-  (err, _) => {
-    if (err) {
-      console.log(err);
-    } else {
-      server();
-    }
-  }
-);
+const serverApollor = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-function server() {
-  const serverApollor = new ApolloServer({
-    typeDefs,
-    resolvers,
-    // context: ({req}) => {
-    //   console.log(req.headers);
-    // }
-  });
-
-  serverApollor.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-
-    // CORS
-  });
-}
-
-// USAR CORS
+serverApollor.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
