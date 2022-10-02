593c6bf428c42e863b8e51dcab9a9de912bbb47a
ricardoTellezA <lian.redi@hotmail.com>
2022-10-02 02:48:36 -0500

    Nose

--- index.js
+++ index.js
@@ -11,8 +11,30 @@ conectarDB();
 const serverApollor = new ApolloServer({
   typeDefs,
   resolvers,
-});
+  context : ({req}) => {
+    // console.log(req.headers['authorization']);
+    const token = req.headers['authorization'] || '';
+    console.log(req.headers);
 
+    if(token){
+      try {
+        const usuario = jwt.verify(token.replace('Bearer ', ''), process.env.SECRETO);
+        console.log(usuario);
+
+
+        return {
+          usuario
+        }
+        
+      } catch (error) {
+
+        console.log(error);
+        
+      }
+    }
+  }
+  
+});
 serverApollor.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
