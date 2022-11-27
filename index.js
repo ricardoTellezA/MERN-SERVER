77db629937d2edbc11f6a685d408999060f55da1
ricardoTellezA <lian.redi@hotmail.com>
2022-11-26 22:50:55 -0600

    add vercel file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./gql/schema");
 const resolvers = require("./gql/resolvers");
 const conectarDB = require("./config/DB");
+const Keyv = require("keyv");
+const { KeyvAdapter } = require("@apollo/utils.keyvadapter");
 require("dotenv").config({
   path: ".env",
 });
@@ -11,29 +13,28 @@ conectarDB();
 const serverApollor = new ApolloServer({
   typeDefs,
   resolvers,
-  context : ({req}) => {
+  cache: new KeyvAdapter(new Keyv()), // cache
+  context: ({ req }) => {
     // console.log(req.headers['authorization']);
-    const token = req.headers['authorization'] || '';
+    const token = req.headers["authorization"] || "";
     console.log(req.headers);
 
-    if(token){
+    if (token) {
       try {
-        const usuario = jwt.verify(token.replace('Bearer ', ''), process.env.SECRETO);
+        const usuario = jwt.verify(
+          token.replace("Bearer ", ""),
+          process.env.SECRETO
+        );
         console.log(usuario);
 
-
         return {
-          usuario
-        }
-        
+          usuario,
+        };
       } catch (error) {
-
         console.log(error);
-        
       }
     }
-  }
-  
+  },
 });
 serverApollor.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
